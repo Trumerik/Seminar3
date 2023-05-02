cb5a63e57189acc1f40602e387bdaca69bfe2932
Erik Hellman <60819181+Trumerik@users.noreply.github.com>
2023-05-02 18:50:33 +0200

    continued work

--- ProcessSale/se/kth/iv1350/processSale/integration/ExternalAccountingSystem.java
+++ ProcessSale/se/kth/iv1350/processSale/integration/ExternalAccountingSystem.java
@@ -1,7 +1,10 @@
 package se.kth.iv1350.processSale.integration;
-
 import se.kth.iv1350.processSale.model.Receipt;
-
+/**
+ * The external accounting system contains accounting information. Calls are only made 
+ * to the class from the 
+ * external systems. All calls to the model pass through here.
+ */
 public class ExternalAccountingSystem {
     
     public ExternalAccountingSystem(){}
