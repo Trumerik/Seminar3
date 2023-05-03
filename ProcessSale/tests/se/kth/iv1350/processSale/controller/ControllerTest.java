5bf7a7d4d478afed3360d15dfaba82611d40cb6a
Erik Hellman <60819181+Trumerik@users.noreply.github.com>
2023-05-03 21:38:29 +0200

    Done with tests

--- ProcessSale/tests/se/kth/iv1350/processSale/controller/ControllerTest.java
+++ ProcessSale/tests/se/kth/iv1350/processSale/controller/ControllerTest.java
@@ -1,13 +1,71 @@
 package ProcessSale.tests.se.kth.iv1350.processSale.controller; 
 
 import org.junit.*;
+
+import se.kth.iv1350.processSale.controller.Controller;
+import se.kth.iv1350.processSale.integration.ReceiptPrinter;
+import se.kth.iv1350.processSale.model.dto.CurrentSaleStatusDTO;
+
 import static org.junit.Assert.assertEquals;
-
+/**
+ * Tests on the methods belonging to the Controller class. Tests on the set/gets methods
+ * are not included, according to the task instructions.
+ */
 public class ControllerTest {
-
-    @Test
-    public startSaleTest() {
-
+    
+    private Controller controller;
+    private ReceiptPrinter receiptPrinter;
+    /**
+     * a setup is made before the tests are made
+     */
+    @Before
+    public void setUp(){
+        receiptPrinter = new ReceiptPrinter();
     }
     
+    /**
+     * test on the method testStartSale
+     */
+    @Test
+    public void testStartSale() {
+        controller = new Controller(receiptPrinter);
+        controller.startSale();
+        assert(true);
+    }
+    /**
+     * test on the method entersItemIdentifier
+     */
+    @Test
+    public void entersItemIdentifierTest() {
+        controller = new Controller(receiptPrinter);
+        controller.startSale();
+        CurrentSaleStatusDTO cssDTO = controller.entersItemIdentifier("mandariner");
+        assertEquals(cssDTO.getItemDescription().getName(), "mandariner");
+        assertEquals(cssDTO.getItemDescription().getVatRate(), 0.25f, 0);
+        assertEquals(cssDTO.getItemDescription().getPrice(), 5, 0);
+        assertEquals(cssDTO.getRunningTotal(), 6.25, 0);
+    }
+    /**
+     * test on the method endSaleTest
+     */
+     @Test
+    public void endSaleTest() {
+        controller = new Controller(receiptPrinter);
+        controller.startSale();
+        controller.entersItemIdentifier("mandariner");
+        float totalPrice = controller.endSale();
+        assertEquals(totalPrice, 6.25, 0);
+    }
+    /**
+     * test on the method enterPayment
+     */
+    @Test
+    public void testEnterPayment(){
+        controller = new Controller(receiptPrinter);
+        controller.startSale();
+        controller.entersItemIdentifier("mandariner");
+        controller.endSale();
+        float change = controller.enterPayment(100f);
+        assertEquals(change, 93.75, 0);
+    }
 }
